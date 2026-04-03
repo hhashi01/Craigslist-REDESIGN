d568ec1dc9be838582dc56402e7f186f20aeec45
zjenkin2 <zjenkin2@charlotte.edu>
2026-04-03 16:38:18 -0400

    Dynamic Header / Footer fixes

--- header_footer.js
+++ header_footer.js
@@ -1,7 +1,20 @@
+function loadStyle(href) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = href;
+  document.head.appendChild(link);
+}
+
 fetch('navigations/header.html')
   .then(res => res.text())
-  .then(html => document.getElementById('nav-placeholder').innerHTML = html);
+  .then(html => {
+    document.getElementById('nav-placeholder').innerHTML = html;
+    loadStyle('header.css');
+  });
 
 fetch('navigations/footer.html')
   .then(res => res.text())
-  .then(html => document.getElementById('footer-placeholder').innerHTML = html);
+  .then(html => {
+    document.getElementById('footer-placeholder').innerHTML = html;
+    loadStyle('footer.css');
+  });
